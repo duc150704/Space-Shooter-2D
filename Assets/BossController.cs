51ad66448c2da72ba7f5a446fcb9f7c8d85fd55a
duc150704 <nvduc150704@gmail.com>
2025-05-09 10:15:26 +0700

    boss1_1

--- Assets/BossController.cs
+++ Assets/BossController.cs
@@ -1,20 +1,97 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BossController : MonoBehaviour
 {
 
     [SerializeField] Transform rightGun;
     [SerializeField] Transform leftGun;
+    [SerializeField] Transform middleGun;
 
-    private string currentState;
+    [SerializeField] List<GameObject> listProjectiles = new List<GameObject>();
+    GameObject player;
 
-    public string CurrentState
+
+
+
+    private void Start()
     {
-        get => currentState;
-        set => currentState = value;
+        player = GameObject.FindObjectOfType<PlayerShooting>().gameObject;
+        //StartCoroutine(Shoot0());
     }
 
+    public void Shoot()
+    {
+        int ShootingType = Random.Range(0,2);
+        switch(ShootingType)
+        {
+            case 0:
+                
+                break;
+            default:
+                break;
+        }
+    }
 
+    IEnumerator Shoot0()
+    {
+        float speed = 10f;
+        for(int i = 0; i < 10; i++)
+        {
+            CreateProjectile(listProjectiles[0], leftGun.position, player.transform.position - leftGun.position, speed);
+            CreateProjectile(listProjectiles[0], rightGun.position, player.transform.position - rightGun.position, speed);
+            yield return new WaitForSeconds(1.5f);
+        }
+    }
+
+    public IEnumerator Shoot1()
+    {
+        float speed = 8f;
+        Coroutine rotate = StartCoroutine(RotateMiddleGun());
+        for(int i = 0; i < 45; i++)
+        {
+            CreateProjectile(listProjectiles[1], middleGun.position, middleGun.transform.up, speed);
+            CreateProjectile(listProjectiles[1], middleGun.position, middleGun.transform.up + middleGun.transform.right, speed);
+            CreateProjectile(listProjectiles[1], middleGun.position, -middleGun.transform.up, speed);
+            CreateProjectile(listProjectiles[1], middleGun.position, middleGun.transform.up - middleGun.transform.right, speed);
+            CreateProjectile(listProjectiles[1], middleGun.position, middleGun.transform.right, speed);
+            CreateProjectile(listProjectiles[1], middleGun.position, -middleGun.transform.up + middleGun.transform.right, speed);
+            CreateProjectile(listProjectiles[1], middleGun.position, -middleGun.transform.right, speed);
+            CreateProjectile(listProjectiles[1], middleGun.position, -middleGun.transform.right - middleGun.transform.up, speed);
+            yield return new WaitForSeconds(0.2f);
+        }
+        StopCoroutine(rotate);
+        if(middleGun)
+            middleGun.rotation = Quaternion.Euler(0f , 0f, 0f);
+    }
+
+    IEnumerator RotateMiddleGun()
+    {
+        float rotatingSpeed = 30f;
+        while (gameObject)
+        {
+            middleGun.gameObject.transform.Rotate(0f, 0f, rotatingSpeed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    void CreateProjectile(GameObject projectile,Vector3 position ,Vector3 direction, float projectileSpeed)
+    {
+        if (projectile == null)
+        {
+            return;
+        }
+
+        float rotateAngles = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+        GameObject go = Instantiate(projectile, position, Quaternion.Euler(0, 0, 180 - rotateAngles));
+
+        Rigidbody2D rb = go.GetComponent<Rigidbody2D>();
+        if (rb)
+        {
+            rb.velocity = projectileSpeed * direction.normalized;
+        }
+        Destroy(go, 6f);
+    }
 }
--- Assets/Prefabs/Waves/Level_1/Path1_3.cs
+++ Assets/Prefabs/Waves/Level_1/Path1_3.cs
@@ -5,19 +5,47 @@ using UnityEngine;
 
 public class Path1_3 : Path
 {
-    GameObject Boss;
+    public enum BossState { moving, shooting }
+
+    GameObject boss;
     [SerializeField] float bossMoveSpeed;
+
+    private BossState currentState = BossState.moving;
     // Start is called before the first frame update
     void Start()
     {
-        Boss = Instantiate(wave.EnermyPrefs[0], wave.PathPoints[0].position, Quaternion.identity);
+        boss = Instantiate(wave.EnermyPrefs[0], wave.PathPoints[0].position, Quaternion.identity);
+        StartCoroutine(IntroState());
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Boss)
-            Boss.gameObject.transform.position = Vector3.MoveTowards(Boss.gameObject.transform.position, wave.PathPoints[1].position, bossMoveSpeed * Time.deltaTime);
+            
+    }
+
+    IEnumerator IntroState()
+    {
+        float time = 3f;
+        float timeCounter = 0f;
+        Rigidbody2D rigidbody2D = boss.GetComponent<Rigidbody2D>();
+
+        while (boss && timeCounter <= time)
+        {
+            boss.transform.position = Vector3.Lerp(wave.PathPoints[0].position, wave.PathPoints[1].position, timeCounter / time);
+            timeCounter += Time.deltaTime;
+            yield return null;
+        }
+        currentState = BossState.shooting;
+        StartCoroutine(AttackState());
+    }
+
+    IEnumerator AttackState()
+    {
+        yield return new WaitUntil(() => currentState == BossState.shooting);
+        BossController bs = boss.GetComponent<BossController>();
+        StartCoroutine(bs.Shoot1());
+        yield return null;
     }
 
 }
